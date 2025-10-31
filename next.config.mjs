615d5ebf459be0dfa3e02ac5d362ca7829d09173
jang won <cjw7378@gmail.com>
2025-10-31 12:27:15 +0900

    배포준비

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Storybook 파일들을 빌드에서 제외
-  webpack: (config) => {
+  pageExtensions: ["tsx", "ts", "jsx", "js"],
+  webpack: (config, { isServer }) => {
+    // Storybook 파일들을 완전히 무시
     config.module.rules.push({
       test: /\.stories\.(js|jsx|ts|tsx)$/,
       use: "ignore-loader",
     });
+
+    // Storybook 파일들을 resolve에서도 제외
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+
     return config;
   },
 };
